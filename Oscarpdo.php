80754310f63cf6586d1d34fcd419efef857bb7f8
SilverClaws02 <72350529764@szily.hu>
2025-03-11 11:45:38 +0100

    Oscarpdo.php elkészült

---
+++ Oscarpdo.php
@@ -0,0 +1,34 @@
+<?php
+require("dbvezerlopdo.php");
+
+class Oscar {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = new DBController();
+    }
+
+    public function getAllOscars() {
+        $query = "SELECT m_ID, title, m_desc, pic FROM movie";
+        return $this->db->executeSelectQuery($query); 
+    }
+
+    public function getOscarsById($oscarId): array {
+        $query = "SELECT m_ID, title, m_desc, pic FROM movie WHERE m_ID = ?";
+        return $this->db->executeSelectQuery(query: $query, params: [$oscarId]);
+    }
+
+    public function getOscarsByType($Mt_name): array {
+        $query = "SELECT m_ID, title, m_desc, pic, movie_type.Mt_description
+                  FROM movie
+                  INNER JOIN movie_type ON movie_type.mt_ID = movie.m_type
+                  WHERE movie_type.Mt_name = ?";
+        return $this->db->executeSelectQuery(query: $query, params: [$Mt_name]);
+    }
+
+    public function __destruct() {
+        $this->db->closeDB();
+    }
+}
+?>
